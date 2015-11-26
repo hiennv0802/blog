a62bf6e1e9e89704a6bdb235f39d871319e5e1ef
hiennv94 <hiennv.th@gmail.com>
2015-11-26 16:59:53 +0700

    Finished blog app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 100.times do |n|
   name  = Faker::Name.name
-  email = "hiennv-#{n+1}@gmail.com"
-  password = "123456"
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
   User.create!(name:  name,
                email: email,
                password:              password,
